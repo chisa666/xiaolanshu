50869517ce07f0f74997b9e796c6aae693a5feb1
chisa <chisa@users.noreply.github.com>
2026-09-17 01:25:11 +0800

    fix: run gateway on reactive stack

--- src/main/java/com/quanxiaoha/xiaolanshu/gateway/xiaolanshuGatewayApplication.java
+++ src/main/java/com/quanxiaoha/xiaolanshu/gateway/xiaolanshuGatewayApplication.java
@@ -1,16 +1,17 @@
 package com.quanxiaoha.xiaolanshu.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = cn.dev33.satoken.spring.SaTokenContextRegister.class)
 public class xiaolanshuGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(xiaolanshuGatewayApplication.class);
         application.setAdditionalProfiles("gateway");
+        application.setWebApplicationType(WebApplicationType.REACTIVE);
         application.run(args);
     }
 
 }
-
